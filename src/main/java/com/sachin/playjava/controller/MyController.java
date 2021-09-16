4b00b0d5aac56b7f369b1fe2d2bd83c9d294ad36
Sachin Bhuibar <sachinbhuibar@gmail.com>
2021-09-16 11:14:19 +0530

    version 1

---
+++ src/main/java/com/sachin/playjava/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.sachin.playjava.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class MyController {
+	@GetMapping("/test-docker")
+	public String getData() {
+		return "in docker tutorial project play java: version 1";
+	}
+}
+
+
